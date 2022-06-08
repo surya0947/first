542cbb44f78cb049e064db7bddcf33cd4eb044e3
surya <surya143.t@gmail.com>
2022-06-08 23:36:48 +0530

    Added Home

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import WelcomeWidgetsDropdown from '../widgets/WelcomeWidgets'
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h1> Welcome Ramarao.K </h1>
+        <h1>Welcome Jennifer</h1>
+        <WelcomeWidgetsDropdown />
       </div>
     )
   }
