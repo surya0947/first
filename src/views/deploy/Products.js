7365e2693f52e04bd669c49c6853b7c82d68103c
madhukirans <mseelam@informatica.com>
2022-06-09 23:21:00 +0530

    test

--- src/views/deploy/Products.js
+++ src/views/deploy/Products.js
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { CCard, CCardBody, CCardGroup, CCardHeader, CWidgetStatsC } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+import React, {useEffect, useState} from 'react'
+import {CCard} from '@coreui/react'
 import axios from 'axios'
-import { cilBasket, cilChartPie, cilPeople, cilSpeedometer, cilUserFollow } from '@coreui/icons'
 import ProductCard from './ProductCard'
 
 const Products = () => {
   const [products, setProducts] = useState({})
 
   useEffect(() => {
-    let url = `${process.env.REACT_APP_API_URL}/getProducts`
+    let url = `${process.env.REACT_APP_API_URL}/idpops/api/v1/getProducts`
     axios
       .get(url)
       .then((response) => {
@@ -22,13 +20,11 @@ const Products = () => {
   }, [])
 
   return (
-    <>
-      <CCard className="mb-4">
-        {products && products.length > 0 && products.map((v, i) => {
-          return <ProductCard key={i} data={v} />
-        })}
-      </CCard>
-    </>
+    <div className="col">
+      {products && products.length > 0 && products.map((v, i) => {
+        return <CCard className="mb-4"> <ProductCard key={i} data={v}/> </CCard>
+      })}
+    </div>
   )
 }
 
